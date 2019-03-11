4cda27d1d629d38f4bdb84ec29392ed0bd3c1518
Xiexe <Jcmaddox1@gmail.com>
2019-03-11 00:02:52 -0400

    Change version number in prep for 2.0 release

--- Editor/XSStyles.cs
+++ Editor/XSStyles.cs
@@ -6,7 +6,7 @@ using System.IO;
 [InitializeOnLoad]
 public class XSStyles : MonoBehaviour
 {
-    public static string ver = "2.0b5";
+    public static string ver = "2.0";
 
     //Help URLs
     public static string mainURL = "https://docs.google.com/document/d/1xJ4PID_nwqVm_UCsO2c2gEdiEoWoCGeM_GDK_L8-aZE/edit#bookmark=id.xh0nk8x7ws1g";
